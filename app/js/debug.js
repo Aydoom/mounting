0a4b2175ac3ca75a1dd4821948db1460cbfe6704
Pavel A.D <aydoom@li.ru>
2017-08-08 22:48:25 +0300

    change #07

--- app/js/debug.js
+++ app/js/debug.js
@@ -1,12 +1,54 @@
+var PageBg = "img/Mountains Landing Page.png";
+
 $(document).ready(function(){
-	$(".test").on("click", function(){
+	console.log("debag start");
+
+	// Create button
+	$(".section").first().before('<button class="ayTest">OFF</button>');
+
+	// Load styles
+	$(".ayTest").css({
+  		position: 'fixed',
+  		top: '0',
+  		left: '0',
+  		zIndex: '100',
+  		opacity: '1',
+  		width: '35px',
+  		backgroundColor: '#FFF',
+  		color: '#000',
+  		margin: '0',
+  		padding: '0',
+  		fontSize: '10px',
+  		border: '2px solid #000',
+  		height: '35px',
+  		lineHeight: '35px'
+	});
+	$('html').css({
+   		backgroundRepeat: 'no-repeat',
+    	backgroundPosition:  '50% 0'
+	})
+
+	// button hover
+	$(".ayTest").bind({
+  		mouseenter: function(e) {
+  			$(this).css('backgroundColor', '#ccc');
+  		},
+  		mouseleave: function(e) {
+  			$(this).css('backgroundColor', '#fff');
+  		}
+	});
+
+	// button click
+	$(".ayTest").bind("click", function(){
 		console.log($("body").css("opacity"));
 		if ($("body").css("opacity") == 1) {
 			$("body").css("opacity", 0.5);
-			$(".test").text("on");
+			$("html").css('backgroundImage', 'url( "' + PageBg + '" )');
+			$(this).text("on");
 		} else {
 			$("body").css("opacity", 1);
-			$(".test").text("off");
+			$("html").css('backgroundImage', 'url()');
+			$(this).text("off");
 		}
 	})
 })
---
+++ app/js/jquery.scroll.js
@@ -0,0 +1,118 @@
+var actSection;
+var mousewheelevt;
+var scrollBlock;
+var countSection;
+
+$(document).ready(function() {
+    countSection = $('.section').length - 1;
+    scrollBlock = false;
+    actSection = getSection();
+    mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel" //FF doesn't recognize mousewheel as of FF3.x
+    
+    arrawUp();
+    activeMenu();
+    
+    console.log(actSection);
+
+    $("body")
+        .css("overflow", "hidden")
+        .bind(mousewheelevt, function(e){
+            if (scrollBlock === false) {
+                console.log(actSection);
+                scrollBlock = true;
+                if(isMoveDown(e)) {
+                    // down
+                    $("body").scrolling(actSection + 1);
+                    arrawUp();
+                } else {
+                    //up
+                    $("body").scrolling(actSection - 1);
+                    arrawUp();
+                } 
+            }
+        
+            e.preventDefault();
+    });
+});
+
+
+function isMoveDown(e) {
+    var evt = window.event || e //equalize event object     
+    evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+    var delta = evt.detail ? evt.detail*(-40) : evt.wheelDelta; //check for detail first, because it is used by Opera and FF
+    
+    return (delta <= 0);
+}
+
+
+function getSection() {
+    var min;
+    var actY = $(window).scrollTop();
+    console.log("actY=>" + actY);
+    
+
+
+    for (var i = $('.section').length - 1; i >= 0; i--) {
+        min = $(".section").eq(i).offset().top;
+        console.log("i=>" + i + "[" + min + ']');
+        if (actY >= min) {
+            console.log("actI => " + i);
+            return i;
+        }
+    }
+}
+
+
+function arrawUp() {
+    if (actSection > 0) {
+        $(".arrowUp").show().on("click", function(){
+            $('body').scrolling(0);
+        });
+    } else {
+        $(".arrowUp").hide();
+    }
+}
+
+
+function activeMenu() {
+    $(".nav-hero li").on("click", function(e){
+        $(".nav-hero li").removeClass("active");
+        $(this).addClass("active");
+        var href = $(this).find("a").attr("href");
+        var parts = href.split("#");
+
+        for (var i = $('.section').length - 1; i >= 0; i--) {
+            if($(".section").eq(i).hasClass('section-' + parts[1])) {
+                $('body').scrolling(i);
+                i = -1;
+            }
+        }
+
+        e.preventDefault();
+    })
+}
+
+
+// Функиця скроллинга
+(function($) {
+  $.fn.scrolling = function(section){
+    actSection = section;
+
+    if (actSection > countSection) {
+        actSection = countSection;
+        scrollBlock = false;
+    } else if (actSection < 0) {
+        actSection = 0;
+        scrollBlock = false;
+    } else {
+        var offset = $(".section").eq(actSection).offset();
+        if (offset.top !== undefined) {
+            $(this).animate({scrollTop: offset.top}, 'slow', function(){
+                scrollBlock = false;
+            });
+        }
+    }
+
+    return this;
+  };
+})(jQuery);
